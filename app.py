f24c44e32d49019d500f5c7d13e45b986c437353
Vikas R Shetty <shettyvikas03@gmail.com>
2024-01-15 17:33:45 +0530

    updated UI

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app=application
 def index():
     return render_template('index.html') 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -43,5 +43,5 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)        
+    app.run(host="0.0.0.0")        
 
